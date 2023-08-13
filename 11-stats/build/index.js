88a472be1bd29e02bc6fcc74b05e12e96e361536
kdjordan <k.dean.jordan@gmail.com>
2023-08-13 12:32:32 -0700

    v1 refactor complete

--- 11-stats/build/index.js
+++ 11-stats/build/index.js
@@ -1,14 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const CsvFileReader_1 = require("./CsvFileReader");
+const MatchReader_1 = require("./MatchReader");
 const MatchResult_1 = require("./MatchResult");
-const reader = new CsvFileReader_1.CsvFileReader('football.csv');
+const reader = new MatchReader_1.MatchReader('football.csv');
 reader.read();
-console.log(reader.data[0][0]);
 let manUnitedWins = 0;
 for (let match of reader.data) {
     if ((match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HomeWin) || (match[2] === 'Man United' && match[5] === MatchResult_1.MatchResult.AwayWin)) {
         manUnitedWins++;
     }
 }
-// console.log(`Number of Man Wins : ${manUnitedWins}`)
+console.log(`Number of Man Wins : ${manUnitedWins}`);
