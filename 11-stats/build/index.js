32ae06620bae07cd81d0689e9e74d054ebb8ee7a
kdjordan <k.dean.jordan@gmail.com>
2023-08-13 12:47:20 -0700

    refactorv2 with interfaces

--- 11-stats/build/index.js
+++ 11-stats/build/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const MatchReader_1 = require("./MatchReader");
+const MatchReader_1 = require("./inheritance/MatchReader");
 const MatchResult_1 = require("./MatchResult");
 const reader = new MatchReader_1.MatchReader('football.csv');
 reader.read();
